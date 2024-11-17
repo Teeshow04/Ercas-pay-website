49d6d15025ddd9108e39c7dc567ed3a200646133
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2024-11-17 08:19:21 +0100

    Describe the changes you made

---
+++ home/app.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+        header.classList.remove("default");
+    } else {
+        header.classList.remove("scrolled");
+        header.classList.add("default");
+    }
+});
