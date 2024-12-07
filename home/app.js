80c49569f62d44a85e7fba6d27a0dcc7cd5735f1
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2024-12-07 03:41:39 +0100

    updated the navbar and toggle on small screen

--- home/app.js
+++ home/app.js
@@ -1,4 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
 const header = document.querySelector("header");
+const  navToggle = document.querySelector('.nav-toggle');
+  const navbar = document.querySelector('.navbar');
+  const menu = document.querySelector('.menu');
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -10,6 +14,21 @@ window.addEventListener("scroll", () => {
     }
 });
 
+navToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    menu.classList.toggle("active");
+
+    // Toggle between the hamburger and close icons
+    const toggleIcon = navToggle.querySelector("i");
+    if (navbar.classList.contains("active")) {
+      toggleIcon.classList.remove("fa-bars");
+      toggleIcon.classList.add("fa-times");
+    } else {
+      toggleIcon.classList.remove("fa-times");
+      toggleIcon.classList.add("fa-bars");
+    }
+  });
+});
 
 
 
