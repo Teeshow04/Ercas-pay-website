bb3aff9244f9e0f8135980286ed7f5b80db85c93
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2024-12-16 20:31:02 +0100

    update on the pricing small screen responsiveness

--- pricing/app.js
+++ pricing/app.js
@@ -1,38 +1,38 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector("header");
-    const  navToggle = document.querySelector('.nav-toggle');
-    const navbar = document.querySelector('.navbar');
-    const menu = document.querySelector('.menu');
-    const checkoutNav = document.querySelector(".checkout-link");
+  const header = document.querySelector("header");
+  const navToggle = document.querySelector('.nav-toggle');
+  const navbar = document.querySelector('.navbar');
+  const menu = document.querySelector('.menu');
+  const checkoutNav = document.querySelector(".checkout-link");
   const payinsNav = document.querySelector(".payins-link");
   const settlementNav = document.querySelector(".settlement-link");
-    
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 50) {
-            header.classList.add("scrolled");
-            header.classList.remove("default");
-        } else {
-            header.classList.remove("scrolled");
-            header.classList.add("default");
-        }
-    });
-    
-    navToggle.addEventListener("click", () => {
-        navbar.classList.toggle("active");
-        menu.classList.toggle("active");
-    
-        // Toggle between the hamburger and close icons
-        const toggleIcon = navToggle.querySelector("i");
-        if (navbar.classList.contains("active")) {
-          toggleIcon.classList.remove("fa-bars");
-          toggleIcon.classList.add("fa-times");
-        } else {
-          toggleIcon.classList.remove("fa-times");
-          toggleIcon.classList.add("fa-bars");
-        }
-      });
-  
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+      header.classList.remove("default");
+    } else {
+      header.classList.remove("scrolled");
+      header.classList.add("default");
+    }
+  });
+
+  navToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    menu.classList.toggle("active");
+
+    // Toggle between the hamburger and close icons
+    const toggleIcon = navToggle.querySelector("i");
+    if (navbar.classList.contains("active")) {
+      toggleIcon.classList.remove("fa-bars");
+      toggleIcon.classList.add("fa-times");
+    } else {
+      toggleIcon.classList.remove("fa-times");
+      toggleIcon.classList.add("fa-bars");
+    }
+  });
+
 
   const checkoutSection = document.getElementById("checkout");
   const payinsSection = document.getElementById("payins");
@@ -56,8 +56,8 @@ document.addEventListener('DOMContentLoaded', () => {
     checkoutSection.style.display = "flex"; // Show Checkout section
     checkoutNav.classList.add("active"); // Highlight Checkout nav
   });
-   // Event listener for Pay-ins navigation
-   payinsNav.addEventListener("click", (event) => {
+  // Event listener for Pay-ins navigation
+  payinsNav.addEventListener("click", (event) => {
     event.preventDefault(); // Prevent default link behavior
     hideAllSections(); // Hide all sections
     payinsSection.style.display = "flex"; // Show Pay-ins section
@@ -77,5 +77,5 @@ document.addEventListener('DOMContentLoaded', () => {
   checkoutSection.style.display = "flex"; // Show Checkout section
   checkoutNav.classList.add("active"); // Set Checkout nav as ac
 });
-    
+
 
